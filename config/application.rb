0633936804414460a032c3f5c0237d0d6f8cd6f6
unknown <ammar_nasikwala@PS7153.persistent.co.in>
2012-08-27 14:57:39 +0530

    railties issue

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 require 'orm_adapter'
 require 'devise/orm/active_record'
 require 'rails/all'
+require 'rails/commands'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
