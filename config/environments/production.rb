5d74c5ee8ee2ad8a4950fd9deed86f034348a836
Ammar Nasikwala <ammar_nasikwala@persistent.co.in>
2013-02-18 17:11:05 +0530

    change for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ BidPal::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.default_url_options = {
-   :host => 'bidpal.herokuapp.com' 
-   #:host => '10.77.112.65:3000' 
+   #:host => 'bidpal.herokuapp.com' 
+   :host => '10.77.112.65:3000' 
   }
   config.action_mailer.delivery_method = :smtp #:sendmail 
   config.action_mailer.smtp_settings = {
