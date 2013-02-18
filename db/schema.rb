df2bc95c20ebc97f464f41e18383d43aedd0a3af
Ammar Nasikwala <ammar_nasikwala@persistent.co.in>
2013-02-18 11:58:05 +0530

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -88,10 +88,6 @@ ActiveRecord::Schema.define(:version => 20130130113324) do
 
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "users_roles", :id => false, :force => true do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-  end
 
   add_index "users_roles", ["user_id", "role_id"], :name => "index_users_roles_on_user_id_and_role_id"
 
