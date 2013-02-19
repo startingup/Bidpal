b2e715c19aac94a0eaeca44bf2f06fec501b159c
Ammar Nasikwala <ammar_nasikwala@persistent.co.in>
2013-02-19 15:49:13 +0530

    db changes

--- db/migrate/20130125063222_rolify_create_roles.rb
+++ db/migrate/20130125063222_rolify_create_roles.rb
@@ -1,12 +1,5 @@
 class RolifyCreateRoles < ActiveRecord::Migration
   def change
-    create_table(:roles) do |t|
-      t.string :name
-      t.references :resource, :polymorphic => true
-
-      t.timestamps
-    end
-
     create_table(:users_roles, :id => false) do |t|
       t.references :user
       t.references :role
